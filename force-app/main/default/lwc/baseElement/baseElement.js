24bb4b7fec57d647bf53bbde32dd6679d87d0f29
Cho-Geer <165348533+Cho-Geer@users.noreply.github.com>
2024-07-09 07:59:48 +0000

    a

--- force-app/main/default/lwc/baseElement/baseElement.js
+++ force-app/main/default/lwc/baseElement/baseElement.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Cho-Geer 165348533+Cho-Geer@users.noreply.github.com
- * @Date: 2024-07-09 07:37:48
- * @LastEditors: Cho-Geer 165348533+Cho-Geer@users.noreply.github.com
- * @LastEditTime: 2024-07-09 07:55:24
- * @FilePath: /chogeer/force-app/main/default/lwc/baseElement/baseElement.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { LightningElement } from "lwc";
 
 export default class BaseElement extends LightningElement {
