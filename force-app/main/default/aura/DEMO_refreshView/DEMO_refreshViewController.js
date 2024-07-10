a01d08589999fc0afc6ec4baeeeaace364c61d83
Geer Cho <zhaoge.tzx@gmail.com>
2024-07-10 13:03:16 +0000

    DEMO_refreshView aura component created

--- force-app/main/default/aura/DEMO_refreshView/DEMO_refreshViewController.js
+++ force-app/main/default/aura/DEMO_refreshView/DEMO_refreshViewController.js
@@ -1,3 +1,11 @@
 ({
-  myAction: function () {}
+  createContact: function (cmp, event) {
+    event.preventDefault();
+    var fields = event.getParam("fields");
+    fields.AccountId = cmp.get("v.recordId");
+    cmp.find("myForm").submit(fields);
+  },
+  handleSuccess: function () {
+    $A.get("e.force:refreshView").fire();
+  }
 });
